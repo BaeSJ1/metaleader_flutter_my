9aca111a248f41d182afb3286392596f19e7d0f8
DESKTOP-3JD1Q3N\itnj <intj.sjbae@gmail.com>
2024-01-15 16:58:23 +0900

    git commit2

---
+++ lib/controller/home_controller.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_inappwebview/flutter_inappwebview.dart';
+import 'package:get/get.dart';
+
+import '../data/user_info.dart';
+import '../model/repository.dart';
+
+class HomeController extends GetxController
+    with GetSingleTickerProviderStateMixin {
+  RxString initPageUrl = ''.obs;
+  Repository repository = Repository();
+  final GlobalKey webViewKey = GlobalKey();
+  InAppWebViewController? webViewController;
+  RxList<WebStateInfo> webStateList = RxList([]);
+  RxList<Widget> tabs = RxList([]);
+  final url = TextEditingController();
+
+  addTabs() {
+    tabs.add(Container(
+
+      width: 120,
+      child: Row(
+        children: [
+          Text('탭'),
+          SizedBox(width: 58,),
+          IconButton(onPressed: (){
+            removeTabs();
+          }, icon: Icon(Icons.close, color: Colors.black,))
+        ],
+      ),
+    )
+    );
+  }
+
+  removeTabs() {
+    tabs.removeAt(0);
+  }
+
+ goToURL(String url){
+    webViewController?.loadUrl(urlRequest: URLRequest(url: WebUri(url)));
+ }
+
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,57 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_inappwebview/flutter_inappwebview.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:get/get.dart';
+import 'package:metaleader/util/colors.dart';
 
-class MyApp extends StatefulWidget {
-  const MyApp({super.key});
+import 'view/home.dart';
 
-  @override
-  State<MyApp> createState() => _MyAppState();
+final localhostServer = InAppLocalhostServer(documentRoot: 'assets');
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  if (!kIsWeb && defaultTargetPlatform == TargetPlatform.android) {
+    await InAppWebViewController.getDefaultUserAgent();
+    //setWebContentsDebuggingEnabled(kDebugMode);
+  }
+
+  if (!kIsWeb) {
+    await localhostServer.start();
+  }
+  runApp(const MyApp());
 }
 
-class _MyAppState extends State<MyApp> {
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return GetMaterialApp(
-      title: 'Luncher',
-      theme: ThemeData(
-        colorScheme:  ColorScheme.fromSeed(seedColor: brandColor),
-        useMaterial3: true,
-      ),
 
+    return GetMaterialApp(
+      title: 'WEB VIEW',
+      color: Colors.white,
+      localizationsDelegates: const [
+        GlobalMaterialLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate
+      ],
+      supportedLocales: const [
+        Locale('ko', 'KR'),
+      ],
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+          fontFamily: "NotoSansCKJ",
+          iconTheme: const IconThemeData(
+            color: CustomColors.iconColor,
+          ),
+          textTheme: const TextTheme(
+              displayMedium: TextStyle(color: CustomColors.textColor))),
+      initialRoute: '/home',
+      getPages: [
+        GetPage(
+            name: '/home', page: () => Home(), binding: HomeBinding()),
+      ],
     );
   }
 }
