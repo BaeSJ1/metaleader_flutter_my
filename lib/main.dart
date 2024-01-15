4c1e32bb62e7e39c6355f69c698145b0f752465b
DESKTOP-3JD1Q3N\itnj <intj.sjbae@gmail.com>
2024-01-15 11:40:36 +0900

    git commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,56 +1,23 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_inappwebview/flutter_inappwebview.dart';
-import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:get/get.dart';
-import 'package:metaleader/util/colors.dart';
-import 'package:metaleader/view/home/home.dart';
 
-final localhostServer = InAppLocalhostServer(documentRoot: 'assets');
-
-Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  if (!kIsWeb && defaultTargetPlatform == TargetPlatform.android) {
-    await InAppWebViewController.getDefaultUserAgent();
-    //setWebContentsDebuggingEnabled(kDebugMode);
-  }
-
-  if (!kIsWeb) {
-    await localhostServer.start();
-  }
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  State<MyApp> createState() => _MyAppState();
+}
 
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
     return GetMaterialApp(
-      title: 'WEB VIEW',
-      color: Colors.white,
-      localizationsDelegates: const [
-        GlobalMaterialLocalizations.delegate,
-        GlobalCupertinoLocalizations.delegate,
-        GlobalWidgetsLocalizations.delegate
-      ],
-      supportedLocales: const [
-        Locale('ko', 'KR'),
-      ],
-      debugShowCheckedModeBanner: false,
+      title: 'Luncher',
       theme: ThemeData(
-          fontFamily: "NotoSansCKJ",
-          iconTheme: const IconThemeData(
-            color: CustomColors.iconColor,
-          ),
-          textTheme: const TextTheme(
-              displayMedium: TextStyle(color: CustomColors.textColor))),
-      initialRoute: '/home',
-      getPages: [
-        GetPage(
-            name: '/home', page: () => Home(), binding: HomeBinding()),
-      ],
+        colorScheme:  ColorScheme.fromSeed(seedColor: brandColor),
+        useMaterial3: true,
+      ),
+
     );
   }
 }
